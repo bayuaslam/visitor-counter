c000b2f33fcf1620b01f2706cd9991b26b4178a4
bayuaslam <aslambavarian@gmail.com>
2026-08-12 11:52:58 +0700

    Load LabHub 2026 redesign theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import AuthGate from "./AuthGate";
 import "./styles.css";
+import "./redesign.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
